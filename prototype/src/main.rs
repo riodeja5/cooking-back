b581eb04cd57a62a330e707b2c942fdbe1333fb3
Shota <riodeja.5.shota@gmail.com>
2023-11-27 22:23:27 +0900

    apply to render

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,13 +17,35 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port_key = "PORT";
+    let default_port = 3000;
+    let port = match env::var(port_key) {
+        Ok(val) => match val.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => {
+                println!(
+                    "the port number \"{}\" is invalid. default port will be used.",
+                    val
+                );
+                default_port
+            }
+        },
+        Err(_) => {
+            println!(
+                "\"{}\" is not defined in enviroment variables. default port will be used.",
+                port_key
+            );
+            default_port
+        }
+    };
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
